26920f03d6eb8846ae6b9ff809e6557321ce1e9c
KaloudasDev <157753309+KaloudasDev@users.noreply.github.com>
2026-02-20 01:40:18 +0200

    Feat: Add GitHub Pages deployment workflow

--- next.config.mjs
+++ next.config.mjs
@@ -3,5 +3,10 @@ const nextConfig = {
     domains: ['i.imgur.com', 'cdn.discordapp.com'],
     unoptimized: true,
   },
+  output: 'export',
+  trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/kaloudasdev-links' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/kaloudasdev-links/' : '',
 };
+
 export default nextConfig;
